0c9aaafca77775e4b42c53f5cd2788d08130aeb2
katiego <katiegaudin@gmail.com>
2015-07-06 17:03:54 -0700

    initial commit

---
+++ script.js
@@ -0,0 +1,66 @@
+$(function() {
+
+    //form to search spotify API
+    var $spotifySearch = $('#spotify-search');
+
+    //form input for track (song)
+    var $track    = $('#track');
+
+    //underscore function to compile the html template
+    var songTemplate = _.template($('#songDisplay').html());
+
+    //element to hold results from spotify API
+    var $results = $('#results');
+
+
+    // submit form to searchs spotify API
+    $spotifySearch.on('submit', function(event) {
+        event.preventDefault();
+        console.log('form submit');
+
+        $('#results').empty();
+
+		var queryString = 'https://api.spotify.com/v1/search?type=track&q=' + $track.val(); 
+		console.log(queryString) 
+        
+	    var songName; 
+	    var songArtist;
+	    var songData;
+
+        $.get(
+          queryString, 
+          function(data) {
+          	var itemsArray = data;
+            // return itemsArray
+			console.log(itemsArray);
+           
+	         _.each(itemsArray.tracks.items, function(items, i) {
+	         	songName = itemsArray.tracks.items[i].name;
+	         	songArtist = itemsArray.tracks.items[i].artists[0].name;
+	            songData = {name: songName, artist: songArtist}
+	            console.log(songData);
+
+
+        		var songList = [{name: "katie", artist: "zoe"}];
+				console.log(songList) 
+
+				//store song name and artist
+				songList.push(songData);
+				console.log(songList);
+				var index = songList.indexOf(songData);
+
+				//append song name and artist to the page
+				var $song = $(songTemplate(songData));
+				$results.append($song);
+	          	});
+		   }
+		);
+
+        $spotifySearch[0].reset();
+        $('#track').focus();
+
+    });                                   
+});
+
+
+
